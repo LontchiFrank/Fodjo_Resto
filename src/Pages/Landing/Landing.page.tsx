1ea77987f49b7e09fceb8c38c911d2038ab86f25
LontchiFrank <fodjof6@gmail.com>
2023-05-21 20:47:48 +0100

    aded react-reveal

--- src/Pages/Landing/Landing.page.tsx
+++ src/Pages/Landing/Landing.page.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Navbar } from "../../Components";
 import styles from "./Landing.module.css";
-import welcome from "../../assets/Group 5.png";
+import welcome from "../../assets/salt.png";
 
 function Landing() {
   return (
     // container mx-auto px-4
     <div className={`${styles.fullscreen}`}>
       <Navbar />
-      <div className="flex " style={{ height: "100vh" }}>
-        <div className="w-2/5 flex flex-col pt-56 px-24">
+      <div
+        className={`${styles.full} lg:flex-row md:flex-col sm:flex-col xs:flex-col`}
+      >
+        <div className="w-2/5 flex flex-col  pt-56 px-24">
           <div className="flex pb-8">
             {" "}
-            <h1 className={styles.landing_header_h1}>afrikan </h1>
-            <span className={styles.k}>foods</span>
+            <h1
+              className={`${styles.landing_header_h1}  sm:text-[80px] lg:text-[120px] `}
+            >
+              afrikan{" "}
+            </h1>
+            <span className={`${styles.k} sm:text-[80px] lg:text-[120px] `}>
+              foods
+            </span>
           </div>
           <h2 className={styles.h2__opensans}>
             Fresh and tasty afrikan food.A pinch of passion in every dish,
@@ -29,8 +37,10 @@ function Landing() {
             <h3 className={styles.h3_price}> 2000frs</h3>
           </div>
         </div>
-        <div className={`w-3/5`}>
-          <div className={styles.welcome}></div>
+        <div className={`w-3/5 xs:w-full sm:w-full `}>
+          <div className={styles.welcome}>
+            <img src={welcome} />
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+// const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
-    extend: {},
+    screens: {
+      xs: "240px",
+
+      sm: "768px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "1024px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1624px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1980px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
+
   plugins: [],
 };
