27c66d8b943394248054f9f66b67a2e2cfaebd07
LontchiFrank <fodjof6@gmail.com>
2023-05-26 12:20:18 +0100

    greet you

--- src/Components/WorkCards/WorkCard.component.tsx
+++ src/Components/WorkCards/WorkCard.component.tsx
@@ -10,9 +10,14 @@ interface WorkProps {
 
 function WorkCard({ titles, name, icon, des }: WorkProps) {
   return (
-    <div className={`${styles.work} flex-col justify-center items-center`}>
-      <div> {icon}</div>
-      <div>{titles} </div>
+    <div
+      className={`${styles.work} p-10 flex flex-col justify-center items-center`}
+    >
+      <div className="pb-12"> {icon}</div>
+      <div className={`${styles.fontSec} text-2xl font-bold pb-6`}>
+        {titles}{" "}
+      </div>
+      <div className={`${styles.fontSec} text-center`}>{des} </div>
     </div>
   );
 }
--- src/Pages/Landing/Landing.page.tsx
+++ src/Pages/Landing/Landing.page.tsx
@@ -39,7 +39,7 @@ function Landing() {
           </h2>
           <div className="flex gap-4">
             <button
-              className={`${styles.button1} text-white font-bold lg:py-6 lg:px-16 md:px-4 md:py-2 md:text-[16px] xs:px-4 xs:py-6 rounded lg:text-[20px]`}
+              className={`${styles.button1} bg-orange-400 text-white font-bold lg:py-6 lg:px-16 md:px-4 md:py-2 md:text-[16px] xs:px-4 xs:py-6 rounded lg:text-[20px]`}
             >
               Order Now
             </button>
@@ -57,67 +57,60 @@ function Landing() {
           </div>
         </div>
       </div>
-      <div
-        className={`${styles.second} h-full flex lg:flex-row md:flex-row sm:flex-col-reverse xs:flex-col-reverse `}
-      >
-        <div className="lg:w-3/5  sm:w-full xs:w-full lg:justify-content-end md:justify-content-end  sm:justify-content-center ">
-          <div className="lg:pt-72 lg:px-28 md:pt-56 md:px-16  ">
-            <img
-              src={meat}
-              className={styles.img_shad}
-              style={{ width: "100%", height: "40%" }}
-            />
-          </div>
-        </div>
-
-        <div className="lg:w-2/5  flex flex-col  sm:px-10 xs:px-12  lg:pt-[27rem] lg:pr-20 md:pt-[20rem] md:pr-12">
-          <h3
-            className={`${styles.fontSec} lg:text-3xl md:text-xl xs:text-xl text-orange-500 font-bold lg:pb-6 md:pb-4 sm:pb-4 xs:pb-3`}
-          >
-            About
-          </h3>
-          <h2
-            className={`${styles.fontSec} lg:text-4xl md:text-2xl font-bold lg:pb-8 md:pb-4 sm:pb-4 xs:pb-3`}
-          >
-            Food Is An Important Part Of A Balance Diet
-          </h2>
-          <p
-            className={`${styles.fontSec} md:text-[12px] sm:text-[12px] xs:pb-4 md:pb-4 lg:pb-8 text-stone-950 lg:text-[20px]`}
-          >
-            A healthy diet rich in fruits, vegetables, whole grains and low-fat
-            dairy can help to reduce your risk of heart disease by maintaining
-            blood pressure and cholesterol levels. <br />
-            <br /> High blood pressure and cholesterol can be a symptom of too
-            much salt and saturated fats in your diet.
-            <br />
-            <br /> Eating a portion of oily fish - such as salmon and trout -
-            each week can also help to lower your risk of developing heart
-            disease. The high levels of omega-3 fatty acids in oily fish are
-            good for heart health.
-          </p>
-          <div className="flex gap-4">
-            <button
-              className={`${styles.fontSec} bg-orange-400 text-white font-bold lg:py-4 lg:px-6 md:px-4 md:py-2 md:text-[16px] sm:text-[14px] xs:text-[9px] xs:px-4 xs:py-6 rounded-lg lg:text-[20px]`}
-            >
-              Learn More
-            </button>
-            <h3
-              className={`${styles.h3_price} text-5xl md:text-4xl xs:text-3xl`}
-            >
-              {" "}
-            </h3>
-          </div>
-        </div>
-      </div>
-
-      <div className="relative w-full h-96 flex flex-col">
-        {/* <div> */}
-        {/* <h3
-          className={`${styles.fontSec} lg:text-3xl md:text-xl xs:text-xl text-orange-500 font-bold lg:pb-6 md:pb-4 sm:pb-4 xs:pb-3`}
+      <div className={`${styles.carrySecond} flex flex-col`}>
+        <div
+          className={`${styles.second} h-full flex lg:flex-row md:flex-row sm:flex-col-reverse xs:flex-col-reverse `}
         >
-          Work
-        </h3> */}
-        <div className="absolute -left-4 -top-[28rem] h-full w-full flex-col flex justify-center items-center ">
+          <div className="lg:w-3/5  lg:h-screen md:h-96 sm:w-full xs:w-full lg:justify-content-end md:justify-content-end  sm:justify-content-center ">
+            <div className="lg:pt-72 lg:px-28 md:pt-56 md:px-16  ">
+              <img
+                src={meat}
+                className={styles.img_shad}
+                style={{ width: "100%", height: "40%" }}
+              />
+            </div>
+          </div>
+
+          <div className="lg:w-2/5 lg:h-[25rem] md:h-96 flex flex-col  sm:px-10 xs:px-12  lg:pt-[27rem] lg:pr-20 md:pt-[20rem] md:pr-12">
+            <h3
+              className={`${styles.fontSec} lg:text-3xl md:text-xl xs:text-xl text-orange-500 font-bold lg:pb-6 md:pb-4 sm:pb-4 xs:pb-3`}
+            >
+              About
+            </h3>
+            <h2
+              className={`${styles.fontSec} lg:text-4xl md:text-2xl font-bold lg:pb-8 md:pb-4 sm:pb-4 xs:pb-3`}
+            >
+              Food Is An Important Part Of A Balance Diet
+            </h2>
+            <p
+              className={`${styles.fontSec} md:text-[12px] sm:text-[12px] xs:pb-4 md:pb-4 lg:pb-8 text-stone-950 lg:text-[20px]`}
+            >
+              A healthy diet rich in fruits, vegetables, whole grains and
+              low-fat dairy can help to reduce your risk of heart disease by
+              maintaining blood pressure and cholesterol levels. <br />
+              <br /> High blood pressure and cholesterol can be a symptom of too
+              much salt and saturated fats in your diet.
+              <br />
+              <br /> Eating a portion of oily fish - such as salmon and trout -
+              each week can also help to lower your risk of developing heart
+              disease. The high levels of omega-3 fatty acids in oily fish are
+              good for heart health.
+            </p>
+            <div className="flex gap-4">
+              <button
+                className={`${styles.fontSec} bg-orange-400 text-white font-bold lg:py-4 lg:px-6 md:px-4 md:py-2 md:text-[16px] sm:text-[14px] xs:text-[9px] xs:px-4 xs:py-6 rounded-lg lg:text-[20px]`}
+              >
+                Learn More
+              </button>
+              <h3
+                className={`${styles.h3_price} text-5xl md:text-4xl xs:text-3xl`}
+              >
+                {" "}
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div className="relative h-full w-full flex-col flex items-center ">
           <h3
             className={`${styles.fontSec} lg:text-2xl md:text-xl xs:text-xl text-orange-500 font-bold lg:pb-6 md:pb-4 sm:pb-4 xs:pb-3`}
           >
@@ -128,22 +121,28 @@ function Landing() {
           >
             How It Works
           </h2>
-          <div className="px-[38rem]">
+          <div className="lg:px-[38rem]md:px-[29rem]">
             <p
-              className={`${styles.fontSec} md:text-[12px] sm:text-[12px] xs:pb-4 md:pb-4 lg:pb-8 text-center text-stone-950 lg:text-[20px]`}
+              className={`${styles.fontSec} md:text-[12px] sm:text-[12px] xs:pb-4 md:pb-4 lg:pb-8 text-center text-stone-950 lg:text-[20px] `}
             >
               A healthy diet rich in fruits, vegetables, whole grains and
               low-fat dairy can help to reduce your risk of heart disease by
               maintaining blood pressure and cholesterol levels. <br />
             </p>
           </div>
-          <div className="w-full h-96 px-32 flex justify-center items-center gap-12">
+          <div className="w-full h-96 px-32 pt-16 flex justify-center items-center gap-12">
             {WorkCardData.map((item, index) => (
-              <WorkCard titles={item.titles} icon={item.icon} />
+              <WorkCard titles={item.titles} icon={item.icon} des={item.des} />
             ))}
           </div>
         </div>
       </div>
+      {/* <div> */}
+      {/* <h3
+          className={`${styles.fontSec} lg:text-3xl md:text-xl xs:text-xl text-orange-500 font-bold lg:pb-6 md:pb-4 sm:pb-4 xs:pb-3`}
+        >
+          Work
+        </h3> */}
     </div>
   );
 }
