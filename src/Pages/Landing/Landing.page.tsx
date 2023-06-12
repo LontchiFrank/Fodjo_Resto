c0029b470d3b393f1d949310860f94332158aca7
LontchiFrank <fodjof6@gmail.com>
2023-06-12 11:24:12 +0100

    look

--- src/Pages/Landing/Landing.page.tsx
+++ src/Pages/Landing/Landing.page.tsx
@@ -192,6 +192,32 @@ function Landing() {
               <br />
             </p>
           </div>
+          <div className="max-w-xl py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+            <div className="flex justify-center md:justify-end -mt-16">
+              <img
+                className="w-20 h-20 object-cover rounded-full border-2 border-orange-400"
+                src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+              />
+            </div>
+            <div>
+              <h2
+                className={`${styles.fontSec} text-gray-800 text-3xl font-semibold`}
+              >
+                Design Tools
+              </h2>
+              <p className={`${styles.fontSec} mt-2 text-gray-600`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                dolores deserunt ea doloremque natus error, rerum quas odio
+                quaerat nam ex commodi hic, suscipit in a veritatis pariatur
+                minus consequuntur!
+              </p>
+            </div>
+            <div className="flex justify-end mt-4">
+              <a href="#" className="text-xl font-medium text-indigo-500">
+                Alice Ndeh
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
