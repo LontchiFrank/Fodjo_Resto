683a06917413088779061a01c77d4de3d705bfe4
LontchiFrank <fodjof6@gmail.com>
2023-08-05 11:51:01 +0100

    zero coming

--- src/Components/SubPages/Home/Home.screen.tsx
+++ src/Components/SubPages/Home/Home.screen.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+import { useSelector } from "react-redux";
 type FillProps = {
   open: boolean;
 };
@@ -6,21 +8,28 @@ type FillProps = {
 function Home(props: FillProps) {
   const windowSidebar: any = window.innerWidth;
   console.log(windowSidebar);
+  const admin: any = JSON.parse(localStorage.getItem("user") || "");
+  const adminName: string = admin.restaurant_name;
+
   return (
     <div
       className={`${
         windowSidebar > 450 ? (props.open ? "ml-64 " : "ml-20") : "ml-0"
       } `}
     >
-      <div className=" flex gap-4 bg-gray-200">
+      <div className={`${styles} flex gap-4 bg-gray-200`}>
         <div
           className={`${
             props.open ? "lg:w-[65%]" : " lg:w-[69%]"
           } p-4 h-screen md:w-[100%] sm:w-[100%] xs:w-[100%] `}
         >
           <div className=" rounded-lg dark:border-gray-700 bg-gray-200">
+            <h2 className="lg:text-2xl font-normal">
+              <div className={styles.wel}>Welcome</div>{" "}
+              <p className={`${styles.resto_name} text-2xl`}> {adminName}</p>
+            </h2>
             <div className="grid grid-cols-3 gap-4 mb-4">
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-orange-800">
+              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-green-800">
                 <p className="text-2xl text-gray-400 dark:text-gray-500">
                   <svg
                     className="w-3.5 h-3.5"
