5624f77d8211f1b6c4e4516a6c43a57e3bf5c59d
LontchiFrank <fodjof6@gmail.com>
2023-09-04 17:49:55 +0100

    finish with images

--- src/Components/SubPages/Home/Home.screen.tsx
+++ src/Components/SubPages/Home/Home.screen.tsx
@@ -48,90 +48,57 @@ function Home(props: FillProps) {
           } p-4 md:w-[100%] sm:w-[100%] xs:w-[100%] `}
         >
           <div className=" rounded-lg dark:border-gray-700 bg-gray-200">
-            {/* <h2 className="lg:text-2xl font-normal">
-              <div className={styles.wel}>Welcome</div>{" "}
-              <p className={`${styles.resto_name} text-2xl`}> {adminName}</p>
-            </h2> */}
             <div
-              className={`${styles.back} flex items-center justify-center h-96 mb-4 rounded-lg bg-gray-50 dark:bg-gray-800`}
+              className={`${styles.back} flex items-left justify-left h-96 mb-4 rounded-lg bg-gray-50 dark:bg-gray-800`}
             >
               <p className="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  className="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
+                <h2 className="lg:text-2xl font-normal px-12 pt-12">
+                  <h3 className={`${styles.wel} text-white text-3xl`}>
+                    Welcome
+                  </h3>{" "}
+                  <p className={`${styles.resto_name} text-3xl`}>
+                    {" "}
+                    {adminName}
+                  </p>
+                </h2>
               </p>
             </div>
-            <div className="grid grid-cols-3 gap-4 mb-4">
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-orange-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                  <svg
-                    className="w-3.5 h-3.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </p>
-              </div>
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                  <svg
-                    className="w-3.5 h-3.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </p>
-              </div>
-              <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                  <svg
-                    className="w-3.5 h-3.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </p>
+            <div className="w-[100%]">
+              <div className="flex flex-row   gap-4 mb-4">
+                <div
+                  className={`${styles.ham} flex items-center justify-center h-36 rounded bg-gray-50 dark:bg-orange-800`}
+                >
+                  <p className="text-2xl text-gray-400 dark:text-gray-500">
+                    <svg
+                      className="w-3.5 h-3.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 18 18"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 1v16M1 9h16"
+                      />
+                    </svg>
+                  </p>
+                </div>
+                <div
+                  className={`${styles.gril} flex items-center justify-center h-36 rounded bg-gray-50 dark:bg-orange-800`}
+                >
+                  <p className="text-2xl text-gray-400 dark:text-gray-500"></p>
+                </div>
+                <div
+                  className={`${styles.cream} flex items-center justify-center h-36 rounded bg-gray-50 dark:bg-orange-800`}
+                >
+                  <p className="text-2xl text-gray-400 dark:text-gray-500"></p>
+                </div>
               </div>
             </div>
+
             <div
               className={`${props.open ? "p-5" : "p-10"} ${
                 data.length == 0
